9166713fe04a638babc765fa82ba03e1e140f06e
eunseo <eunseo.choi.d@gmail.com>
2023-08-14 12:18:21 +0900

    .

--- barking_dog/0x07_덱/boj_1021.cpp
+++ barking_dog/0x07_덱/boj_1021.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <deque>
 using namespace std;
 
 int main()
@@ -6,10 +7,37 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, m;
-    cin >> n >> m;
+    int n, m_cnt;
+    int res = 0;
+    deque<int> DQ;
 
-    
+    cin >> n >> m_cnt;
+    for (int i = 1; i <= n; i++)
+    {
+        DQ.push_back(i);
+    }
+    for (int i = 0; i < m_cnt; i++)
+    {
+        int m;
+        cin >> m;
+
+        int idx = 0;
+        while (m != DQ.front())
+        {
+            int tmp = DQ.front();
+            DQ.pop_front();
+            DQ.push_back(tmp);
+            idx++;
+        }
+        DQ.pop_front();
+
+        if (idx < DQ.size() - idx + 1)
+            res += idx;
+        else
+            res += (DQ.size() - idx + 1);
+    }
+
+    cout << res;
 
     return 0;
 }
