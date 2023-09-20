143730f6752d72fd0a1069e25a5851c41754cf55
eunseo <eunseo.choi.d@gmail.com>
2023-09-20 09:38:07 +0900

    DFS

--- barking_dog/0x09_BFS/boj_9466.cpp
+++ barking_dog/0x09_BFS/boj_9466.cpp
@@ -8,45 +8,6 @@ int done[100002];
 int vis[100002];
 vector<int> team;
 
-int find(int num)
-{
-    for (int i = 0; i < team.size(); i++)
-    {
-        if (num == team[i]) return i;
-    }
-    return -1;
-}
-
-void print()
-{
-    for (int i = 1; i <= n; i++)
-    {
-        cout << i << "\t";
-    }
-    cout << endl;
-    for (int i = 1; i <= n; i++)
-    {
-        cout << board[i] << "\t";
-    }
-    cout << endl;
-    for (int i = 1; i <= n; i++)
-    {
-        cout << done[i] << "\t";
-    }
-    cout << endl;
-    for (int i = 1; i <= n; i++)
-    {
-        cout << vis[i] << "\t";
-    }
-    cout << endl;
-    cout << "team > ";
-    for (int i = 0; i < team.size(); i++)
-    {
-        cout << team[i] << " ";
-    }
-    cout << endl;
-}
-
 int main()
 {
     ios::sync_with_stdio(0);
@@ -64,50 +25,39 @@ int main()
             vis[i] = 0;
         }
 
-        // cout << "----------- start -----------" << endl;
-
         for (int i = 1; i <= n; i++)
         {
             if (done[i] == 1 || vis[i] == 1) continue;
-
-            // cout << "i = " << i << endl;
+            team.clear();
 
             int curr = i;
-            team.clear();
             team.push_back(curr);
-
-            // cout << "======== 1 ========" << endl;
-            // print();
-            // cout << "===================" << endl;
+            vis[curr] = 1;
 
             while (1)
             {
-                vis[curr] = 1;
-
                 int next = board[curr];
-                int idx = find(next);
 
-                // cout << "\t next = " << next << endl;
-                // cout << "\t idx = " << idx << endl;
-
-                if (idx == -1)
+                if (vis[next] == 0)
                 {
                     team.push_back(next);
+                    vis[next] = 1;
                     curr = next;
                 }
                 else
                 {
+                    int idx;
+                    for (idx = 0; idx < team.size(); idx++)
+                    {
+                        if (team[idx] == next) break;
+                    }
                     for (int j = idx; j < team.size(); j++)
+                    {
                         done[team[j]] = 1;
+                    }
                     break;
                 }
-                // print();
             }
-
-            // cout << "======== 2 ========" << endl;
-            // print();
-            // cout << "===================" << endl;
-            // cout << endl;
         }
 
         int res = 0;
