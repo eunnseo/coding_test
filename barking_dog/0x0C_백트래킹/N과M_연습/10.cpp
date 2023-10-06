8a04912cce000633c21383d58796072b4c52c330
eunnseo <eunseo.choi.d@gmail.com>
2023-10-06 23:00:18 +0900

    백트래킹

---
+++ barking_dog/0x0C_백트래킹/N과M_연습/10.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int num[10];
+int arr[10];
+
+void bfs(int idx, int st)
+{
+    if (idx == m)
+    {
+        for (int i = 0; i < m; i++)
+            cout << num[arr[i]] << " ";
+        cout << "\n";
+        return;
+    }
+
+    int tmp = 0;
+    for (int i = st; i < n; i++)
+    {
+        if (tmp != num[i])
+        {
+            arr[idx] = i;
+            tmp = num[i];
+            bfs(idx + 1, i + 1);
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> num[i];
+
+    sort(num, num + n);
+
+    bfs(0, 0);
+
+    return 0;
+}
